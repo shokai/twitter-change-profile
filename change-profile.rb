9584de8e42503d088d6594d5ba620e393b5ef3c6
Sho Hashimoto <hashimoto@shokai.org>
2010-08-16 23:54:18 +0900

    update profile

--- change-profile.rb
+++ change-profile.rb
@@ -1,7 +1,38 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'twitter'
+require 'yaml'
 require File.dirname(__FILE__)+'/lib/wikipedia'
-
 $KCODE = 'u'
 
+begin
+  conf = YAML::load open(File.dirname(__FILE__) + '/config.yaml')
+rescue
+  STDERR.puts 'config.yaml load error'
+  exit 1
+end
+
+tw = Twitter::Base.new(Twitter::HTTPAuth.new(conf['name'], conf['pass']))
 w = Wikipedia.new('shokai')
-p w.random
+desc = nil
+5.times do
+  data = w.random
+  
+  desc = data[:descriptions].first
+  desc.gsub!(/\[\d+\]/, '')
+  tmp = desc.split(/(と?は)/)
+  left = tmp.shift
+  while left =~ /（[^）]+$/ do
+    tmp.shift
+    left = tmp.shift
+  end
+  desc = conf['your_name']+tmp.join('')
+  puts '-'*10
+  print data[:name] + ' => '
+  puts desc
+  break if desc != conf['your_name']
+end
+exit if desc == nil or desc == conf['your_name']
+
+tw.update_profile({'description' => desc})
