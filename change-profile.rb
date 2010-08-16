e15bbfcca5cf176356a13a8a885c4f92af9df262
Sho Hashimoto <hashimoto@shokai.org>
2010-08-16 22:42:39 +0900

    first commit

---
+++ change-profile.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__)+'/lib/wikipedia'
+
+$KCODE = 'u'
+
+p Wikipedia.random
---
+++ lib/wikipedia.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'open-uri'
+require 'uri'
+require 'nokogiri'
+require 'kconv'
+require 'net/http'
+
+class Wikipedia
+
+  def initialize(agent_name)
+    @agent_name = agent_name
+  end
+  
+  def get(name)
+    doc = Nokogiri::HTML open(URI.encode("http://ja.wikipedia.org/wiki/#{name}"), 'User-Agent' => @agent_name).read.toutf8
+    
+    title = doc.xpath('//title').first.text
+    name = doc.xpath('//h1').first.text
+    descriptions = doc.xpath('//div[@id="bodyContent"]//p').map{|i|i.text}
+    {
+      :title => title,
+      :name => name,
+      :descriptions => descriptions
+    }
+  end
+
+  def random
+
+  end
+end
